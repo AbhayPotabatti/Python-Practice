a395574f847ec7ecc61a9fb40f55a0e48d8b5f76
AbhayPotabatti <abhaypotabatti2463@gmail.com>
2021-08-14 10:55:08 +0530

    Arithmetic operation

---
+++ Arithmeticoperations.py
@@ -0,0 +1,15 @@
+#Arithmetic operation
+x=10
+y=20
+
+sum=y-x
+
+print("The answer of these 2 numbers is ",sum)
+
+multiplication=x*y
+
+print("THe answer is ",multiplication)
+
+division=y/x
+
+print("The answer is",division)
